aa54aa6c71d16de6f7afcd79e576a9bee75d4888
Himanshu Mote <86561270+himanshumote03@users.noreply.github.com>
2023-06-14 13:27:08 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+package Proj_1;
+
+import java.util.Scanner;
+
+public class Main{
+    public static int majority(int[] arr, int n){
+        int maxCount = 0;
+        int majorityElement = 0;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                majorityElement = arr[i];
+            }
+        }
+        if (majorityElement >= 2) {
+            return majorityElement;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array: ");
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+
+        System.out.println("Enter elements: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Majority element is: " + majority(arr, n));
+    }
+}
